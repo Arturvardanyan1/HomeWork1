664b4db4bd405f8cbef2d39f001b2d14d766c4be
User <User@DESKTOP-Q3NTQEA>
2023-02-10 17:01:00 +0400

    Add project files.

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace HomeWork1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 16;
+            int y = 5;
+            int c = x + y;
+            Console.WriteLine("x " + "+ " + "y " + "= " + c);
+            int q = x - y;
+            Console.WriteLine("x " + "- " + "y " + "= " + q);
+            int k = x * y;
+            Console.WriteLine("x " + "* " + "y " + "= " + k);
+            int t = x / y;
+            Console.WriteLine("x " + "/ " + "y " + "= " + t);
+            int z = x % y;
+            Console.WriteLine("x " + "% " + "y " + "= " + z);
+
+            if (x > y)
+            {
+                y++;
+                Console.WriteLine(y);
+            }
+            else
+            {
+                x--;
+                Console.WriteLine(x);
+            }
+            var a = x < y ? ++x : --y;
+            Console.WriteLine(a);
+
+        }
+    }
+}
